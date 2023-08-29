ad6f37bc763e111f372ca0029673f976fe18f2a9
Claudio R. González <claudio.gonzalez@am53.com.br>
2023-08-29 14:59:08 -0300

    colors theme was modify

--- app/src/main/java/com/ubuntu/mashtempapp/MainActivity.java
+++ app/src/main/java/com/ubuntu/mashtempapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ubuntu.mashtempapp;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -7,19 +8,20 @@ import android.bluetooth.BluetoothSocket;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.graphics.Typeface;
+import android.media.MediaPlayer;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.*;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.core.app.ActivityCompat;
 
 import java.io.BufferedReader;
@@ -30,53 +32,30 @@ import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView minNumberTextView;
+    private TextView maxNumberTextView;
+    private int currentMinValue = 70;
+    private int currentMaxValue = 74;
+
     private BluetoothSocket bluetoothSocket;
     private InputStream inputStream;
     private TextView temperatureTextView;
-
-//    private Button button_inc;
-//    private Button button_dec;
+    private MediaPlayer mediaPlayer;
+    private Ringtone ringtone;
 
     private ProgressBar progress_temp;
+
+    private SwitchCompat betAlarmSwitch;
+    private SwitchCompat uppAlarmSwitch;
 
     // Este es el código que identifica la solicitud de permiso de Bluetooth
     private static final int BT_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // TextView para mostar la temperatura
-        temperatureTextView = findViewById(R.id.progress_temp_text);
-        // Carga la fuente personalizada desde la carpeta assets/fonts
-        Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/gothanthin.otf");
-
-        // Aplica la fuente al TextView
-        temperatureTextView.setTypeface(customFont);
-
-        progress_temp =  findViewById(R.id.progress_temp);
-        progress_temp.setMax(10000);
-
-        progress_temp.setProgress(2536);
-        temperatureTextView.setText("25.36°");
-
-//        button_inc = findViewById(R.id.button_inc);
-//        button_dec = findViewById(R.id.button_dec);
-//
-//        button_inc.setOnClickListener(v -> {
-//            if(progr <= 100){
-//                progr += 1;
-//                updateProgressBar();
-//            }
-//        });
-//
-//        button_dec.setOnClickListener(v -> {
-//            if(progr >= 0){
-//                progr -= 1;
-//                updateProgressBar();
-//            }
-//        });
 
         BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (bluetoothAdapter == null) {
@@ -92,12 +71,12 @@ public class MainActivity extends AppCompatActivity {
         UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
         try {
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
                 // No tienes el permiso, así que necesitas solicitarlo.
                 ActivityCompat.requestPermissions(
                         this,
                         new String[]{
-                                android.Manifest.permission.BLUETOOTH_CONNECT
+                                Manifest.permission.BLUETOOTH_CONNECT
                         }, BT_PERMISSION_CODE);
             } else {
                 // Tienes el permiso, puedes continuar con tus operaciones relacionadas con Bluetooth.
@@ -116,6 +95,118 @@ public class MainActivity extends AppCompatActivity {
             }
         } catch (IOException | SecurityException e) {
             Log.e("Error: {}", e.getMessage());
+        }
+
+        // TextView para mostar la temperatura
+        temperatureTextView = findViewById(R.id.progress_temp_text);
+        // Carga la fuente personalizada desde la carpeta assets/fonts
+        Typeface customFont = Typeface.createFromAsset(getAssets(), "fonts/gothanthin.otf");
+
+        // textos del termometro muestra min y max temp
+        TextView minTemp = findViewById(R.id.minTextView);
+        TextView maxTemp = findViewById(R.id.maxTextView);
+
+        // Aplica la fuente a los TextView
+        temperatureTextView.setTypeface(customFont);
+        minTemp.setTypeface(customFont);
+        maxTemp.setTypeface(customFont);
+
+        // set minimo y máximo
+        progress_temp =  findViewById(R.id.progress_temp);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            progress_temp.setMin(-5500);
+        }
+        progress_temp.setMax(12500);
+
+        // esto es temporario se setean valores fijos para el termometro
+//        progress_temp.setProgress(7853);
+//        temperatureTextView.setText("78.53°");
+
+        // botones y texto configura temperatura minima
+        minNumberTextView = findViewById(R.id.minTextNumber);
+        Button decMinButton = findViewById(R.id.dec_min_button);
+        Button incMinButton = findViewById(R.id.inc_min_button);
+
+        decMinButton.setOnClickListener(v -> {
+            if (currentMinValue > 0) {
+                currentMinValue--;
+                updateMinNumberTextView();
+            }
+        });
+
+        incMinButton.setOnClickListener(v -> {
+            currentMinValue++;
+            updateMinNumberTextView();
+        });
+
+        // botones y texto configura temperatura minima
+        maxNumberTextView = findViewById(R.id.maxTextNumber);
+        Button decMaxButton = findViewById(R.id.dec_max_button);
+        Button incMaxButton = findViewById(R.id.inc_max_button);
+
+        decMaxButton.setOnClickListener(v -> {
+            if (currentMaxValue > 0) {
+                currentMaxValue--;
+                updateMaxNumberTextView();
+            }
+        });
+
+        incMaxButton.setOnClickListener(v -> {
+            currentMaxValue++;
+            updateMaxNumberTextView();
+        });
+
+        // Switch para alarmas
+        betAlarmSwitch = findViewById(R.id.betAlarmSwitch);
+        uppAlarmSwitch = findViewById(R.id.uppAlarmSwitch);
+
+        // Configurar la alarma cuando el estado del Switch cambia
+        betAlarmSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                playAlarmSound(RingtoneManager.TYPE_NOTIFICATION );
+            } else {
+                cancelAlarmSound();
+            }
+        });
+
+        // Configurar la alarma cuando el estado del Switch cambia
+        uppAlarmSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                playAlarmSound(RingtoneManager.TYPE_ALARM);
+            } else {
+                cancelAlarmSound();
+            }
+        });
+
+    }
+
+    private void updateMinNumberTextView() {
+        String textWithDegree = currentMinValue + "°";
+        minNumberTextView.setText(textWithDegree);
+    }
+
+    private void updateMaxNumberTextView() {
+        String textWithDegree = currentMaxValue + "°";
+        maxNumberTextView.setText(textWithDegree);
+    }
+
+    private void playAlarmSound(Integer type) {
+        Toast.makeText(this, "Alarm ON", Toast.LENGTH_SHORT).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(type);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+        }
+
+        // Reproduce el sonido
+        ringtone = RingtoneManager.getRingtone(this, alarmUri);
+        ringtone.play();
+    }
+
+    private void cancelAlarmSound() {
+        Toast.makeText(this, "Alarm Off", Toast.LENGTH_SHORT).show();
+
+        if (ringtone != null && ringtone.isPlaying()) {
+            ringtone.stop();
         }
     }
 
@@ -205,19 +296,30 @@ public class MainActivity extends AppCompatActivity {
 
                 String receivedData = (String) msg.obj;
 
-                updateProgressBar(receivedData);
+                double progress = Double.parseDouble(receivedData);
+
+                updateProgressBar((int) progress * 100);
+
+                checkAlarm((int)progress);
 
             }
         }
 
         @SuppressLint("SetTextI18n")
-        private void updateProgressBar(String str){
+        private void updateProgressBar(int value){
 
-            double progr = Double.parseDouble(str) * 100;
-            int aux = (int) progr;
+            progress_temp.setProgress(value);
+            temperatureTextView.setText(value + "°");
 
-            progress_temp.setProgress(2536);
-            temperatureTextView.setText("25.36");
+        }
+
+        private void checkAlarm(int value){
+
+            if(value > currentMaxValue && uppAlarmSwitch.isChecked()){
+                playAlarmSound(RingtoneManager.TYPE_ALARM );
+            } else if (value >= currentMinValue && value < currentMaxValue && betAlarmSwitch.isChecked()){
+                playAlarmSound(RingtoneManager.TYPE_NOTIFICATION);
+            }
 
         }
 
